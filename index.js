85ffa57ccfc66cbb4b9031cf50dee0108c5ad96c
Tim Malone <tdmalone@gmail.com>
2017-10-19 18:15:06 +1100

    WIP: Tests & verify pass-thru function

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 
 'use strict';
 
-module.exports = function() {
+/**
+ * Takes a string of HTML and an optional ruleset. Passes to and returns the result of HTMLHint's
+ * verify function, after inserting the bundled shared config. User rules override those provided
+ * by the shared config.
+ *
+ * @param {string} html    A string of HTML to lint.
+ * @param {object} ruleset An optional HTMLHint ruleset provided by the user.
+ * @return {array} An array of message objects from the HTMLHint linter.
+ */
+function verify( html, ruleset ) {
 
-  // Coming soon.
+  const HTMLHint = require( 'htmlhint' ).HTMLHint;
 
-}; // Module.exports.
+  let userRuleset = ruleset;
+  let sharedConfig = require( './rules' ).rules;
+
+  if ( 'undefined' === typeof ruleset ) {
+    userRuleset = {};
+  }
+
+  // Override any options in the sharedConfig with options the user has supplied.
+  Object.assign( sharedConfig, userRuleset );
+
+  // Run HTMLHint.
+  return HTMLHint.verify( html, sharedConfig );
+
+} // Function verify.
+
+module.exports = {};
+
+module.exports.HTMLHint = require( 'htmlhint' ).HTMLHint;
+module.exports.HTMLHint.verify = verify;
--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,46 @@
 
 'use strict';
 
-const path = require( 'path' );
+const path = require( 'path' ),
+      isPlainObj = require( 'is-plain-obj' );
+
 const FILENAME = path.basename( __filename );
 
-test( FILENAME + ' exports a function', () => {
+test( FILENAME + ' exports an object', () => {
   const module = require( '../' + FILENAME );
-  expect( 'function' === typeof module ).toBe( true );
+  expect( isPlainObj( module ) ).toBe( true );
+});
+
+test( 'HTMLHint loads', () => {
+  const module = require( '../' + FILENAME );
+  expect( isPlainObj( module.HTMLHint ) ).toBe( true );
+});
+
+test( 'A sample HTMLHint native function (addRule) is correctly available', () => {
+  const module = require( '../' + FILENAME );
+  expect( 'function' === typeof module.HTMLHint.addRule ).toBe( true );
+});
+
+test( 'The HTMLHint verify function returns an expected array of message objects', () => {
+
+  const module = require( '../' + FILENAME );
+  const result = module.HTMLHint.verify( '<ul><li></li>', { 'tag-pair': true });
+  const FIRST_ARRAY_INDEX = 0;
+
+  expect( Array.isArray( result ) ).toBe( true );
+  expect( isPlainObj( result[ FIRST_ARRAY_INDEX ]) ).toBe( true );
+  expect( 'string' === typeof result[ FIRST_ARRAY_INDEX ].message ).toBe( true );
+
+});
+
+test( 'HTMLHint.verify() correctly uses bundled shared config', () => {
+  const module = require( '../' + FILENAME );
+});
+
+test( 'HTMLHint.verify() allows overriding of bundled shared config', () => {
+  const module = require( '../' + FILENAME );
+});
+
+test( 'HTMLHint.verify() accepts bundled custom rules', () => {
+  const module = require( '../' + FILENAME );
 });
